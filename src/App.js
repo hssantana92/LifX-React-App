cb491a55b3ea7657943cfb2b58dfc5844c87fec7
hssantana92 <86550766+hssantana92@users.noreply.github.com>
2021-08-07 10:21:14 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'semantic-ui-css/semantic.min.css'
 import React from 'react';
 import Authorise from './Authorise/Authorise';
+import FlashOnRoundedIcon from '@material-ui/icons/FlashOnRounded';
 
 const logoLink = "https://cdn.shopify.com/s/files/1/0219/0638/files/lifx_logo_9ae0aa6d-07df-41ba-b96d-0f400323e1b1_140x.png?v=1575366323"
 
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1><img src={logoLink} alt="LifX"></img></h1>
+      <h1><i style={{fontSize: "30px", marginBottom: "15px", color: "#ffdb59"}} className="lightbulb icon"></i>LifX Light Controller</h1>
       <Authorise></Authorise>  
     </div>
   );
